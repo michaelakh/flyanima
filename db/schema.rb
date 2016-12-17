616e22248457dc2d697eccd73e1312f5c24125ac
michaelakh <michael_ak@hotmail.co.uk>
2016-12-17 14:25:17 +0000

    Add review, update db for user integration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215164231) do
+ActiveRecord::Schema.define(version: 20161217140412) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20161215164231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "keywords"
+    t.integer  "user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
