7ae8649cec83989160c41d839a653ea27c2d93f9
michaelakh <michael_ak@hotmail.co.uk>
2017-01-06 23:42:32 +0000

    delete useless drop migrations

--- db/migrate/20170101003801_drop_search_suggestions_table.rb
+++
@@ -1,9 +0,0 @@
-class DropSearchSuggestionsTable < ActiveRecord::Migration[5.0]
-  def change
-      drop_table :search_suggestions
-  end
-    
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20170102210440_drop_city_airport_ita_draft_table.rb
+++
@@ -1,9 +0,0 @@
-class DropCityAirportItaDraftTable < ActiveRecord::Migration[5.0]
-  def change
-      drop_table :city_airport_ita
-  end
-    
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
